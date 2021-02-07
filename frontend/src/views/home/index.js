a1b5ad532929b41a9b8bf8cb3bc8567e9eaed614
Ishan Arya <ishanarya0@gmail.com>
2021-02-08 00:54:06 +0530

    building-home-page

--- frontend/src/views/home/index.js
+++ frontend/src/views/home/index.js
@@ -1,8 +1,25 @@
+import { Jumbotron, Button, Image } from "react-bootstrap";
+import image from "../../assets/images/home-page.jpg";
+import image2 from "../../assets/images/Campus-B34st.png";
+
 const Home = () => {
+  console.log(image);
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <Jumbotron
+      style={{
+        flexDirection: "column",
+        flex: 1,
+        justifyContent: "space-between",
+        width: "100%",
+      }}
+    >
+      <Image src={image} style={{ height: 400, width: 600 }} />
+      <Image src={image2} />
+      {"Make your own community or join one!"}
+      <Button variant="primary" style={{ alignSelf: "flex-end" }}>
+        Let's go!
+      </Button>
+    </Jumbotron>
   );
 };
 
