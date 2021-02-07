1316f3a6ae622561b8c4aa68cd7923fd4c589285
Ishan Arya <ishanarya0@gmail.com>
2021-02-07 22:45:50 +0530

    structuring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
-import NavBar from "./components/navigation-bar";
-import CustomRouter from "./components/custom-router";
+import { NavBar, CustomRouter } from "./components";
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
       <CustomRouter />
-      {/* Route components are rendered if the path prop matches the current URL */}
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+import NavBar from "./navigation-bar";
+import CustomRouter from "./custom-router";
+
+export { NavBar, CustomRouter };
