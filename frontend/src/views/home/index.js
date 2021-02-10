e5f8929b7a666d8a685969b4831706edf3510e67
Ishan Arya <ishanarya0@gmail.com>
2021-02-10 12:16:58 +0530

    home-page-enhancements

--- frontend/src/views/home/index.js
+++ frontend/src/views/home/index.js
@@ -1,25 +1,53 @@
-import { Jumbotron, Button, Image } from "react-bootstrap";
+import { Jumbotron, Button, Image, Row, Col } from "react-bootstrap";
+import CustomCard from "../../components/home";
 import image from "../../assets/images/home-page.jpg";
 import image2 from "../../assets/images/Campus-B34st.png";
 
 const Home = () => {
   console.log(image);
   return (
-    <Jumbotron
-      style={{
-        flexDirection: "column",
-        flex: 1,
-        justifyContent: "space-between",
-        width: "100%",
-      }}
-    >
-      <Image src={image} style={{ height: 400, width: 600 }} />
-      <Image src={image2} />
-      {"Make your own community or join one!"}
-      <Button variant="primary" style={{ alignSelf: "flex-end" }}>
-        Let's go!
-      </Button>
-    </Jumbotron>
+    <>
+      <Jumbotron
+        style={{
+          flexDirection: "column",
+          flex: 1,
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Image
+          src={image}
+          style={{ height: 400, width: 600, marginRight: 50 }}
+        />
+        <Image src={image2} style={{ marginRight: 50 }} />
+        {"Make your own community or join one!"}
+        <Button
+          variant="primary"
+          style={{ alignSelf: "flex-end", marginLeft: 30 }}
+        >
+          Let's go!
+        </Button>
+      </Jumbotron>
+      <Row style={{ marginLeft: 100 }}>
+        <Col>
+          <CustomCard
+            title={"Login"}
+            text={"Login to access all the features"}
+            goto={"/login"}
+          />
+        </Col>
+        <Col>
+          <CustomCard
+            title={"Profile"}
+            text={"Check your profile"}
+            goto={"/profile"}
+          />
+        </Col>
+        <Col>
+          <CustomCard title={"Chat"} text={"Coming Soon"} goto={"/"} />
+        </Col>
+      </Row>
+    </>
   );
 };
 
